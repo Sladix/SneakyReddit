34df8a4915da946d7d9a24c056cc0d802f54a3b1
Antoine <aguillien@kookline.net>
2017-12-21 17:03:17 +0100

    first

--- src/scripts/contentscript.js
+++ src/scripts/contentscript.js
@@ -1,33 +1,56 @@
 import ext from "./utils/ext";
 
-var extractTags = () => {
+// var extractTags = () => {
+//   var url = document.location.href;
+//   if(!url || !url.match(/^http/)) return;
+//
+//   var data = {
+//     title: "",
+//     description: "",
+//     url: document.location.href
+//   }
+//
+//   var ogTitle = document.querySelector("meta[property='og:title']");
+//   if(ogTitle) {
+//     data.title = ogTitle.getAttribute("content")
+//   } else {
+//     data.title = document.title
+//   }
+//
+//   var descriptionTag = document.querySelector("meta[property='og:description']") || document.querySelector("meta[name='description']")
+//   if(descriptionTag) {
+//     data.description = descriptionTag.getAttribute("content")
+//   }
+//
+//   return data;
+// }
+
+var fetchReddit = (path) => {
+
+}
+
+var extractType = () => {
   var url = document.location.href;
-  if(!url || !url.match(/^http/)) return;
-
-  var data = {
-    title: "",
-    description: "",
-    url: document.location.href
+  let type = 'default';
+  if(url.match(/inbox\.google/)){
+    type = 'inbox';
+  }
+  if(url.match(/mantis\./)){
+    type = 'mantis';
   }
 
-  var ogTitle = document.querySelector("meta[property='og:title']");
-  if(ogTitle) {
-    data.title = ogTitle.getAttribute("content")
-  } else {
-    data.title = document.title
-  }
-
-  var descriptionTag = document.querySelector("meta[property='og:description']") || document.querySelector("meta[name='description']")
-  if(descriptionTag) {
-    data.description = descriptionTag.getAttribute("content")
-  }
-
-  return data;
+  return type;
 }
 
 function onRequest(request, sender, sendResponse) {
   if (request.action === 'process-page') {
-    sendResponse(extractTags())
+    let data = {
+      type : extractType()
+    };
+    sendResponse(data);
+  }
+  if (request.action === 'inject') {
+
   }
 }
 
--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -2,6 +2,7 @@ import ext from "./utils/ext";
 import storage from "./utils/storage";
 
 var popup = document.getElementById("app");
+
 storage.get('color', function(resp) {
   var color = resp.color;
   if(color) {
@@ -9,55 +10,65 @@ storage.get('color', function(resp) {
   }
 });
 
-var template = (data) => {
-  var json = JSON.stringify(data);
-  return (`
-  <div class="site-description">
-    <h3 class="title">${data.title}</h3>
-    <p class="description">${data.description}</p>
-    <a href="${data.url}" target="_blank" class="url">${data.url}</a>
-  </div>
-  <div class="action-container">
-    <button data-bookmark='${json}' id="save-btn" class="btn btn-primary">Save</button>
-  </div>
-  `);
-}
-var renderMessage = (message) => {
-  var displayContainer = document.getElementById("display-container");
-  displayContainer.innerHTML = `<p class='message'>${message}</p>`;
-}
-
-var renderBookmark = (data) => {
-  var displayContainer = document.getElementById("display-container")
-  if(data) {
-    var tmpl = template(data);
-    displayContainer.innerHTML = tmpl;  
-  } else {
-    renderMessage("Sorry, could not extract this page's title and URL")
-  }
-}
-
-ext.tabs.query({active: true, currentWindow: true}, function(tabs) {
-  var activeTab = tabs[0];
-  chrome.tabs.sendMessage(activeTab.id, { action: 'process-page' }, renderBookmark);
-});
-
-popup.addEventListener("click", function(e) {
-  if(e.target && e.target.matches("#save-btn")) {
-    e.preventDefault();
-    var data = e.target.getAttribute("data-bookmark");
-    ext.runtime.sendMessage({ action: "perform-save", data: data }, function(response) {
-      if(response && response.action === "saved") {
-        renderMessage("Your bookmark was saved successfully!");
-      } else {
-        renderMessage("Sorry, there was an error while saving your bookmark.");
-      }
-    })
-  }
-});
-
 var optionsLink = document.querySelector(".js-options");
 optionsLink.addEventListener("click", function(e) {
   e.preventDefault();
   ext.tabs.create({'url': ext.extension.getURL('options.html')});
-})
+});
+
+// On click, we gather data about page
+ext.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  var activeTab = tabs[0];
+  chrome.tabs.sendMessage(activeTab.id, { action: 'process-page' }, renderInjectOptions);
+});
+
+// Renders the different inject option based on the page's gathered datas
+var renderInjectOptions = (data) => {
+  var displayContainer = document.getElementById("display-container");
+  displayContainer.innerHTML = `<p>${JSON.stringify(data)}</p>`;
+}
+
+//
+// var template = (data) => {
+//   var json = JSON.stringify(data);
+//   return (`
+//   <div class="site-description">
+//     <h3 class="title">${data.title}</h3>
+//     <p class="description">${data.description}</p>
+//     <a href="${data.url}" target="_blank" class="url">${data.url}</a>
+//   </div>
+//   <div class="action-container">
+//     <button data-bookmark='${json}' id="save-btn" class="btn btn-primary">Save</button>
+//   </div>
+//   `);
+// }
+// var renderMessage = (message) => {
+//   var displayContainer = document.getElementById("display-container");
+//   displayContainer.innerHTML = `<p class='message'>${message}</p>`;
+// }
+//
+// var renderBookmark = (data) => {
+//   var displayContainer = document.getElementById("display-container")
+//   if(data) {
+//     var tmpl = template(data);
+//     displayContainer.innerHTML = tmpl;
+//   } else {
+//     renderMessage("Sorry, could not extract this page's title and URL")
+//   }
+// }
+//
+//
+// popup.addEventListener("click", function(e) {
+//   if(e.target && e.target.matches("#save-btn")) {
+//     e.preventDefault();
+//     var data = e.target.getAttribute("data-bookmark");
+//     ext.runtime.sendMessage({ action: "perform-save", data: data }, function(response) {
+//       if(response && response.action === "saved") {
+//         renderMessage("Your bookmark was saved successfully!");
+//       } else {
+//         renderMessage("Sorry, there was an error while saving your bookmark.");
+//       }
+//     })
+//   }
+// });
+//
